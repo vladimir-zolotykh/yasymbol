a99527acdfcefaa11893eb4279698581b39ff237
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-08-18 17:35:16 +0300

    Add Token, iter_tokens()

--- symbol.py
+++ symbol.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from typing import Iterator
+import re
 
 
 class Symbol:
@@ -22,9 +24,44 @@ class Symbol:
         return cls._instances
 
 
+for name, pat in (
+    ("NAME", r"[A-Za-z_]\w*"),
+    ("NUM", r"\d+"),
+    ("PLUS", r"\+"),
+    ("MINUS", r"-"),
+    ("MUL", r"\*"),
+    ("DIV", r"/"),
+    ("LPAREN", r"\("),
+    ("RPAREN", r"\)"),
+    ("WS", r"\s+"),
+):
+    Symbol(name, pat)
+
+
+class Token:
+    def __init__(self, sym: Symbol, val: float | str | None):
+        self.sym = sym
+        self.val = val
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, type(self)):
+            return self.sym == other.sym and self.val == other.val
+        elif isinstance(other, str):
+            return self.sym.name == other
+        else:
+            return NotImplemented
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.sym}, {self.val!r})"
+
+
+def iter_tokens(sexpr: str, masterpat=Symbol.masterpat()) -> Iterator[Token]:
+    for tok in re.find_iter(masterpat, sexpr):
+        if tok.lastgroup == Symbol("WS"):
+            continue
+        yield Token(Symbol(tok.lastgroup, tok.group(0)))
+
+
 if __name__ == "__main__":
-    NAME = Symbol("NAME", r"[A-Za-z_]\w*")
-    WS1 = Symbol("WS", r"\s+")
-    WS2 = Symbol("WS", r"\s+")
-    assert WS1 is WS2
-    print(Symbol.masterpat())
+    for tok in iter_tokens(""):
+        print(tok)
