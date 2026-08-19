971ea518bb6b5c28dad59195ca8b4a830dbb3645
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-08-19 09:17:17 +0300

    Initial import
    Node classes, Parser class

---
+++ node.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+
+
+class Num(Node):
+    def __init__(self, val: float):
+        super().__init__(val)
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, type(self)):
+            return self.val == other.val
+        else:
+            return NotImplemented
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self.val})"
+
+
+class BinOp(Node):
+    def __init__(self, val: float | str, left: Node, right: Node):
+        super().__init__(val)
+        self.left = left
+        self.right = right
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, type(self)):
+            return (
+                super().__eq__(other)
+                and self.left == other.left  # noqa: W503
+                and self.right == other.right  # noqa: W503
+            )
+        else:
+            return NotImplemented
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.left}, {self.right})"
+
+
+class Plus(BinOp):
+    pass
+
+
+class Minus(BinOp):
+    pass
+
+
+class Mul(BinOp):
+    pass
+
+
+class Div(BinOp):
+    pass
---
+++ parser.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from typing import Iterator
+from node import Node, Num, Plus, Minus, Mul, Div
+from symbol import Symbol, Token, iter_tokens
+
+
+class Parser:
+    def __init__(self):
+        pass
+
+    def parse(self, sexpr) -> Node:
+        self.tokens: Iterator[Token] = iter_tokens(sexpr)
+        self.tok: Token = self._advance()
+        return self.expr()
+
+    def expr(self) -> Node:
+        res = self.term()
+        while (op := self.tok) and op.sym in ("PLUS", "MINUS"):
+            self._consume()
+            right = self.term()
+            res = Plus(res, right) if op == "PLUS" else Minus(res, right)
+        return res
+
+    def term(self) -> Node:
+        res = self.factor()
+        while (op := self.tok) and op.sym in ("MUL", "DIV"):
+            self._consure()
+            right = self.factor()
+            res = Mul(res, right) if op == "MUL" else Div(res, right)
+        return res
+
+    def factor(self) -> Node:
+        if self.tok.sym == "LPAREN":
+            self._consure()
+            res = self.expr()
+            self._expect("RPAREN")
+        else:
+            res = Num(self.tok.val)
+            self._consure
+        return res
+
+    def _advance(self) -> Token:
+        self.tok = next(self.tokens, None)
+        return self.tok
+
+    def _consume(self) -> None:
+        self.tok = next(self.tokens, None)
+
+    def _expect(self, expected: Symbol) -> None:
+        if self.tok.sym != expected:
+            raise SyntaxError(f"{expected!r} expected, got {self.tok.sym!r}")
+        self.tok = next(self.tokens, None)
